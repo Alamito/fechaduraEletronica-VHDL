32986fef9b0ace562a11d49ef79b0f3d0b8c03a0
Alamito <alamirbobroski12@gmail.com>
2023-04-02 19:51:15 -0300

    feat - add display (nao ta pegando direito)

--- proj-final/simulation/qsim/work/reg_decod_reg@user/_primary.vhd
+++ proj-final/simulation/qsim/work/reg_decod_reg@user/_primary.vhd
@@ -3,12 +3,20 @@ use verilog.vl_types.all;
 entity reg_decod_regUser is
     port(
         comp            : out    vl_logic;
+        btn_seleciona   : in     vl_logic;
         reg_user        : out    vl_logic_vector(3 downto 0);
         btn_inc         : in     vl_logic;
         btn_troca       : in     vl_logic;
         CLK             : in     vl_logic;
         change_pass     : in     vl_logic;
-        btn_seleciona   : in     vl_logic;
+        pin_name2       : out    vl_logic;
+        S00             : out    vl_logic;
+        S11             : out    vl_logic;
+        S22             : out    vl_logic;
+        S33             : out    vl_logic;
+        S44             : out    vl_logic;
+        S55             : out    vl_logic;
+        S66             : out    vl_logic;
         S               : out    vl_logic_vector(3 downto 0)
     );
 end reg_decod_regUser;
